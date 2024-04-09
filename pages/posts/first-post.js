14fefeaa4985a82386ba2f278d298e0a94b34cd6
Katleho Ribisi <katlehoribisi@gmail.com>
2024-04-09 09:48:50 +0200

    big change

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,18 +1,18 @@
-import Link from "next/link";
-import Head from "next/head";
-import Layout from "../../components/layout";
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
-    return (
-        <Layout>
-            <Head>
-                <title>First Post</title>
-            </Head>
-
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">Back to home</Link>
-            </h2>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">← Back to home</Link>
+        <Link href="/posts/new-page">← Check out this new page.</Link>
+      </h2>
+    </Layout>
+  );
 }
